e6e21e72a07e168d75907f37e55e12c299ba3193
Awing Ding <awingding.win@gmail.com>
2026-03-29 16:32:37 +0200

    create abstract headers

---
+++ include/AbstractDriver.h
@@ -0,0 +1,72 @@
+//
+// Created by awing on 29/03/2026.
+//
+
+#ifndef MOTORS_ABSTRACTDRIVER_H
+#define MOTORS_ABSTRACTDRIVER_H
+
+#define MAX_MOTOR_IN_DRIVER 2
+
+#include <cstdint>
+
+#include "AbstractMotors.h"
+
+
+/**
+ * This class represent an Abstract Driver.
+ */
+class AbstractDriver {
+public:
+    virtual ~AbstractDriver() = default;
+
+    /**
+     * Attach a motor to the first empty channel available
+     * @param motor the motor to attach
+     * @return the id of the motor
+     */
+    virtual int attach(AbstractMotor* motor) = 0;
+
+    /**
+     * Attach a motor to the specified id
+     * @param motor the motor to attach
+     * @param id the channel id
+     * @return -1 if it failed, the motor's id else
+     */
+    virtual int attach(AbstractMotor* motor, uint8_t id) = 0;
+
+    /**
+     * Detach the motor at this id
+     * @param id the id at which the motor is
+     */
+    virtual void detach(const int id) {
+        m_motors[id] = nullptr;
+    }
+
+    /**
+     * set the velocity of the motor at the specified id
+     * @param velocity the velocity to be set
+     * @param id the motor id
+     */
+    virtual void setVelocity(float velocity, uint8_t id) = 0;
+
+    /**
+     * get the velocity from the motor at the specified id
+     * @param id the motor id
+     * @return the velocity
+     */
+    virtual float getVelocity(uint8_t id) const = 0;
+
+    /**
+     * return a pointer to the motor at the specified id
+     * @param id the motor id
+     * @return a pointer to the motor, nullptr if it doesn't exist
+     */
+    AbstractMotor* getMotor(const int id) const {
+        if (id < 0 || id >= MAX_MOTOR_IN_DRIVER) return nullptr;
+        return m_motors[id];
+    }
+protected:
+    AbstractMotor* m_motors[MAX_MOTOR_IN_DRIVER] = {};
+};
+
+#endif //MOTORS_ABSTRACTDRIVER_H
---
+++ include/AbstractMotors.h
@@ -0,0 +1,45 @@
+//
+// Created by awing on 29/03/2026.
+//
+
+#ifndef MOTORS_ABSTRACTMOTORS_H
+#define MOTORS_ABSTRACTMOTORS_H
+
+#include "AbstractDriver.h"
+
+
+class AbstractMotor
+{
+public:
+    /**
+     * @brief Constructeur d'AbstractMotor.
+     *
+     * Méthode à implémenter.
+     */
+    virtual ~AbstractMotor(){}
+    /**
+     * @brief Charge une nouvelle vitesse.
+     *
+     * Change la vitesse du moteur par celle passée en parametre. Méthode à implémenter.
+     *
+     * @param velocity Nouvelle vitesse.
+     */
+    virtual void setVelocity(float velocity) {
+        this->m_driver->setVelocity(velocity, this->m_driverId);
+    };
+
+    /**
+     * @brief Retourne vitesse max
+     *
+     * Retourne la vitesse max du moteur pour son intégration dans les calculs de DifferentialController.
+     *
+     * @return virtual float
+     */
+    virtual float getMaxVelocity() const = 0;
+
+protected:
+    AbstractDriver* m_driver = nullptr;
+    uint8_t m_driverId = 255;
+
+};
+#endif //MOTORS_ABSTRACTMOTORS_H
